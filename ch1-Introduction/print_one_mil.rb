2ccda22ddc7e183abf76a233eb147965cd5e2cc9
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-04-14 11:48:59 -0400

    Add code to print 100 numbers

--- ch1-Introduction/print_one_mil.rb
+++ ch1-Introduction/print_one_mil.rb
@@ -1,9 +1,9 @@
 # Output the numbers 1 to 1000000, one line at a time, to std out
 
-# file = File.new("one_million.txt", "wtx")
+# 1_000_000.times do |i|
+#   puts(i)
+# end
 
-1_000_000.times do |i|
-  puts(i)
-end
-
-# file.close
+# 100.times do |i|
+#   puts i
+# end
