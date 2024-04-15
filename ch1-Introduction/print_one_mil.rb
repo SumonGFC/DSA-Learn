9eef21f830b36272e4119f4a1220d5a8a7858a0f
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-04-15 14:38:50 -0400

    Update one_million function to accomodate parts 2 and 3 of the exercise

--- ch1-Introduction/print_one_mil.rb
+++ ch1-Introduction/print_one_mil.rb
@@ -1,9 +1,17 @@
 # Output the numbers 1 to 1000000, one line at a time, to std out
 
 # 1_000_000.times do |i|
-#   puts(i)
+#   if rand <= 0.99
+#     puts i
+#   else
+#     puts ""
+#   end
 # end
 
 # 100.times do |i|
-#   puts i
+#   if rand >= 0.9
+#     puts i
+#   else
+#     puts ""
+#   end
 # end
