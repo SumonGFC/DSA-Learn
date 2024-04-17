041966579f2dbcacfe37978ce82baa79fd5ea9ef
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-04-17 12:43:19 -0400

    Update script to print line numbers, duplicate lines, and blank lines

--- ch1-Introduction/print_one_mil.rb
+++ ch1-Introduction/print_one_mil.rb
@@ -1,17 +1,23 @@
 # Output the numbers 1 to 1000000, one line at a time, to std out
 
-# 1_000_000.times do |i|
-#   if rand <= 0.99
-#     puts i
-#   else
-#     puts ""
-#   end
-# end
+dups = ["dup1","dup2","dup3","dup4","dup5","dup6","dup7"]
+
+ 1_000_000.times do |i|
+   if rand <= 0.00005
+     puts i
+   elsif rand <= 0.5         # sprinkle in some duplicate lines
+     puts dups[rand(0..6)]
+   else
+     puts ""                 # sprinkle in some blank lines
+   end
+ end
 
 # 100.times do |i|
-#   if rand >= 0.9
+#   if rand <= 0.8
 #     puts i
+#   elsif rand <= 0.5         # sprinkle in some duplicate lines
+#     puts dups[rand(0..6)]
 #   else
-#     puts ""
+#     puts ""                 # sprinkle in some blank lines
 #   end
 # end
