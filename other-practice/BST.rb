ce3090470199124071513534636d3c320d6640fe
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-06-08 14:17:15 -0400

    Implement build_tree() and insert()

--- other-practice/BST.rb
+++ other-practice/BST.rb
@@ -1,3 +1,42 @@
+# Tree class represents our (balanced) Binary Seach Tree
+class Tree
+  attr_reader :root, :arr
+
+  def initialize(arr)
+    @arr = arr
+  end
+
+  def build_tree(arr)
+    return nil if arr.empty?
+
+    mid = arr.length / 2
+    root = Node.new(arr[mid])
+    root.left = build_tree(arr[0...mid])
+    root.right = build_tree(arr[(mid + 1)..])
+    @root = root
+  end
+
+  def insert(node, curr_node)
+    return curr_node.left = node if curr_node.left.nil? && node < curr_node
+    return curr_node.right = node if curr_node.right.nil? && node > curr_node
+
+    if node < curr_node
+      insert(node, curr_node.left)
+    elsif node == curr_node
+      nil
+    else
+      insert(node, curr_node.right)
+    end
+  end
+
+
+ def pretty_print(node = @root, prefix = '', is_left = true)
+   pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+   puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+   pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+ end
+end
+
 # Node object represents a node in a balanced BST
 class Node
   include Comparable
@@ -22,3 +61,16 @@ class Node
     @data <=> other.data
   end
 end
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+arr3 = [1]
+bst = Tree.new(arr)
+bst.build_tree(bst.arr)
+# p bst.root
+bst.pretty_print
+new_node = Node.new(10)
+new_node2 = Node.new(8.5)
+p bst.insert(new_node, bst.root)
+p bst.insert(new_node2, bst.root)
+p bst.root
+bst.pretty_print
