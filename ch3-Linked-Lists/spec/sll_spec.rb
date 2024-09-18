111d305cced85861e566aa3b46aa75cb048dd65a
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-09-18 12:17:33 -0400

    CH3: change #push to #add_first in SLL spec

--- ch3-Linked-Lists/spec/sll_spec.rb
+++ ch3-Linked-Lists/spec/sll_spec.rb
@@ -3,26 +3,26 @@
 require_relative '../lib/sll.rb'
 
 RSpec.describe SLL do
-  describe '#push' do
+  describe '#add_first' do
     context 'with empty list' do
       let(:empty_list) { described_class.new }
 
       it 'adds new element as head' do
-        expect { empty_list.push(:obj) }
+        expect { empty_list.add_first(:obj) }
           .to change { empty_list.head }
           .from(nil)
           .to(:obj)
       end
 
       it 'changes number of elements from 0 to 1' do
-        expect { empty_list.push(:obj) }
+        expect { empty_list.add_first(:obj) }
           .to change { empty_list.len }
           .from(0)
           .to(1)
       end
 
       it 'tail and head point to same object' do
-        empty_list.push(:obj)
+        empty_list.add_first(:obj)
         expect(empty_list.head).to be (empty_list.tail)
       end
     end
@@ -30,26 +30,26 @@ RSpec.describe SLL do
     context 'with filled list' do
       let(:list) do
         sll = described_class.new
-        100.times { |i| sll.push(i) }
+        100.times { |i| sll.add_first(i) }
         sll
       end
 
       it 'adds new element as head' do
-        expect {list.push(100) }
+        expect {list.add_first(100) }
           .to change { list.head }
           .from(99)
           .to(100)
       end
 
       it 'changes length of list from 100 to 101' do
-        expect {list.push(100) }
+        expect {list.add_first(100) }
           .to change { list.len }
           .from(100)
           .to(101)
       end
 
       it 'does not change tail node' do
-        expect {list.push(100) }.not_to change { list.tail }
+        expect {list.add_first(100) }.not_to change { list.tail }
       end
 
     end
