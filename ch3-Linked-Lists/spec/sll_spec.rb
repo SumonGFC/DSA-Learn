211b9d5890847f624ab8f63ddf03380fff1f9f0f
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-09-19 10:49:46 -0400

    CH3: (SLL) implement #remove_first spec

--- ch3-Linked-Lists/spec/sll_spec.rb
+++ ch3-Linked-Lists/spec/sll_spec.rb
@@ -49,9 +49,89 @@ RSpec.describe SLL do
       end
 
       it 'does not change tail node' do
-        expect {list.add_first(100) }.not_to change { list.tail }
+        expect { list.add_first(100) }.not_to change { list.tail }
+      end
+    end
+  end
+
+  describe '#remove_first' do
+    context 'with empty list' do
+      let(:empty_list) { described_class.new }
+
+      it 'returns nil' do
+        expect(empty_list.remove_first).to be nil
       end
 
+      it 'does not change length of list' do
+        expect { empty_list.remove_first }.not_to change { empty_list.len }
+      end
+
+      it 'leaves head as nil' do
+        expect { empty_list.remove_first }.not_to change { empty_list.head }
+      end
+
+      it 'leaves tail as nil' do
+        expect { empty_list.remove_first }.not_to change { empty_list.tail }
+      end
+
+      it 'leaves the entire list empty' do
+        before = empty_list.to_a
+        empty_list.remove_first
+        after = empty_list.to_a
+        expect(before).to eq after
+      end
     end
+
+    context 'with filled list' do
+      let(:filled_list) do
+        x = described_class.new
+        10.times { |i| x.add_first(i) }
+        x
+      end
+
+      it 'returns head' do
+        head = filled_list.head
+        removed = filled_list.remove_first
+        expect(removed).to be head
+      end
+
+      it 'reduces length of list by 1' do
+        old_len = filled_list.len
+        filled_list.remove_first
+        new_len = filled_list.len
+        expect(old_len).to eq (new_len + 1)
+      end
+
+      it 'modifies head pointer to next item in list' do
+        next_item = filled_list.head_ptr.next
+        filled_list.remove_first
+        expect(next_item).to be filled_list.head_ptr
+      end
+
+      it 'does not change tail' do
+        old_tail = filled_list.tail_ptr
+        filled_list.remove_first
+        new_tail = filled_list.tail_ptr
+        expect(old_tail).to be new_tail
+      end
+    end
+  end
+
+  describe '#add_last' do
+  end
+
+  describe '#remove_last' do
+  end
+
+  describe '#get' do
+  end
+
+  describe '#set' do
+  end
+
+  describe '#contains?' do
+  end
+
+  describe '#find' do
   end
 end
